ff60087e36ab09979543cdb82e2e27bfb5d47a1a
Khusboo <insoftlink.react03@gmail.com>
2025-12-29 10:30:36 +0530

    made folder structure for css

--- app/components/Landing-Components/sub_Components/Feature_Card.jsx
+++ app/components/Landing-Components/sub_Components/Feature_Card.jsx
@@ -2,13 +2,13 @@
 const Feature_Card = ({feature}) => {
   return (
     <div className="feature-card w-48 md:w-60 lg:w-75 rounded-2xl bg-white p-2 md:p-3 lg:p-5">
-      <div className="flex items-center gap-1 lg:gap-3">
+      <div className="flex items-center gap-3 lg:gap-4">
        
         <div>
           <p className="text-[0.5rem] md:text-[0.6rem] lg:text-xs leading-2 sm:leading-3 mb-0 text-gray-800 mt-1">
             {feature.desc}
           </p>
-          <h4 className="font-normal text-[0.6rem] md:text-[0.7rem] lg:text-sm">
+          <h4 className="font-normal mt-1 text-[0.6rem] md:text-[0.7rem] lg:text-[1rem]">
             {feature.title}
           </h4>
           
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import "../public/styles/landing.css"
+import "../public/styles/responsive.css"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
